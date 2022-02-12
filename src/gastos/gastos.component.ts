5d5ebd37e155873d6bf64b6fc5ab3c34d3781892
Cerebrado <nallar.juan@gmail.com>
2022-02-12 20:39:27 +0100

    6 files modified

--- src/gastos/gastos.component.ts
+++ src/gastos/gastos.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-gastos',
   templateUrl: './gastos.component.html',
-  styleUrls: ['./gastos.component.css']
 })
 export class GastosComponent implements OnInit {
 
--- src/stats/stats.component.ts
+++ src/stats/stats.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-stats',
   templateUrl: './stats.component.html',
-  styleUrls: ['./stats.component.css']
 })
 export class StatsComponent implements OnInit {
 
