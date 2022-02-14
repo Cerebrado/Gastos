133bf82e0bca5731a2a800f2bbfc51fb9773e2db
Cerebrado <nallar.juan@gmail.com>
2022-02-14 18:13:30 +0100

    2 files modified

--- src/daily/daily.component.ts
+++ src/daily/daily.component.ts
@@ -7,7 +7,7 @@ import { Month } from '../model';
 })
 export class DailyComponent implements OnInit {
   @Input() public Months: Month[];
-periodSelector: string;
+  periodSelector: string;
 
   // @Output() CurrentPeriodChange: EventEmitter<Period> =
   //   new EventEmitter<Period>();
@@ -15,33 +15,37 @@ periodSelector: string;
   constructor() {}
 
   ngOnInit() {
-    this.periodSelector = "M";
+    this.periodSelector = 'M';
   }
 
   getNumbers(periodsAgo: number, isIncome: boolean) {
-    var today = new Date(); 
-    if(this.periodSelector == "M")
-    {
-      if(periodsAgo == 0)
-      {
+    var today = new Date();
+    if (this.periodSelector == 'M') {
+      if (periodsAgo == 0) {
         let currentMonth = this.selectMonthForDate(today);
-        if(!currentMonth)
-          return 0;
+        if (!currentMonth) return 0;
         return this.calculate(currentMonth, isIncome);
+      }
+
+      if (periodsAgo == 1) {
+        today.setDate(0); //set the date to the latest day of the previous month.
+        let pastMonth = this.selectMonthForDate(today);
+        if (!pastMonth) return 0;
+        return this.calculate(pastMonth, isIncome);
       }
     }
   }
 
-  calculate(currentMonth: Month,isIncome: boolean) {
+  calculate(currentMonth: Month, isIncome: boolean) {
     throw new Error('Method not implemented.');
   }
-      
-      
-  selectMonthForDate(date:Date) {
-    var monthesToday = this.Months.filter((element, index, arr) => element.Year == date.getFullYear() && element.Month == date.getMonth())
-    if(monthesToday.length == 0)
-      return null;
-    return monthesToday[0];
 
+  selectMonthForDate(date: Date) {
+    var monthesToday = this.Months.filter(
+      (element, index, arr) =>
+        element.Year == date.getFullYear() && element.Month == date.getMonth()
+    );
+    if (monthesToday.length == 0) return null;
+    return monthesToday[0];
   }
 }
