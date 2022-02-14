0a6b7559ef2fa8d51840df274e39012e7be888f4
Cerebrado <nallar.juan@gmail.com>
2022-02-14 18:05:08 +0100

    2 files modified

--- src/daily/daily.component.ts
+++ src/daily/daily.component.ts
@@ -1,27 +1,47 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Period } from '../model';
+import { Month } from '../model';
 
 @Component({
   selector: 'app-daily',
   templateUrl: './daily.component.html',
 })
 export class DailyComponent implements OnInit {
-  @Input() public CurrentPeriod: Period;
-  @Output() CurrentPeriodChange: EventEmitter<Period> =
-    new EventEmitter<Period>();
+  @Input() public Months: Month[];
+periodSelector: string;
 
-  @Input() public PastPeriod: Period;
+  // @Output() CurrentPeriodChange: EventEmitter<Period> =
+  //   new EventEmitter<Period>();
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.periodSelector = "M";
+  }
 
   getNumbers(periodsAgo: number, isIncome: boolean) {
-    var period = periodsAgo == 0 ? this.CurrentPeriod : this.PastPeriod;
-    var result = 0;
-    for (let i = 0; i < period.Operations.length; i++) {
-      result += period.Operations[i].Quantity * period.Operations[i].Item.Price;
+    var today = new Date(); 
+    if(this.periodSelector == "M")
+    {
+      if(periodsAgo == 0)
+      {
+        let currentMonth = this.selectMonthForDate(today);
+        if(!currentMonth)
+          return 0;
+        return this.calculate(currentMonth, isIncome);
+      }
     }
-    return result;
+  }
+
+  calculate(currentMonth: Month,isIncome: boolean) {
+    throw new Error('Method not implemented.');
+  }
+      
+      
+  selectMonthForDate(date:Date) {
+    var monthesToday = this.Months.filter((element, index, arr) => element.Year == date.getFullYear() && element.Month == date.getMonth())
+    if(monthesToday.length == 0)
+      return null;
+    return monthesToday[0];
+
   }
 }
